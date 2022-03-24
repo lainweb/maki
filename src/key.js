a8eeee01706b8c563f9f60ca94bbb84b768707c7
leonel alejandro <alejandroo_27_@outlook.com>
2022-03-24 02:08:36 -0300

    entegracion 24/03/2022

--- src/key.js
+++ src/key.js
@@ -1,9 +1,17 @@
 module.exports = {
-    database: {
-        host: 'bxfwbmgh9vi1puv5978e-mysql.services.clever-cloud.com',
-        user: 'unq1x4uyrjlkvrig',
-        password: 'OukCZ3FTfuLYBlXDkQF1',
-        database: 'bxfwbmgh9vi1puv5978e',
-        port: '3306'
-    }
+   database: {
+     host: 'bxfwbmgh9vi1puv5978e-mysql.services.clever-cloud.com',
+      user: 'unq1x4uyrjlkvrig',
+    password: 'OukCZ3FTfuLYBlXDkQF1',
+      database: 'bxfwbmgh9vi1puv5978e',
+      port: '3306'
+  }
 };
+///module.exports = {
+ //   database: {
+  //      host: 'localhost',
+   //     user: 'root',
+  //      password: '',
+ //       database: 'pagina'
+ //   }
+///};
--- src/routes/vistas.js
+++ src/routes/vistas.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const pool = require('../database');
 
-const {isLoggedIn}=require('../lib/auth');
+const { isLoggedIn } = require('../lib/auth');
 
 
 
@@ -12,79 +12,119 @@ const {isLoggedIn}=require('../lib/auth');
 
 router.get('/verEmpresas', async (req, res) => {
 
+
+
+
+
     const empresa = await pool.query('SELECT * FROM empresa ');
-    
-    res.render('vistas/verEmpresas',{empresa});
+
+
+
+    const tipo = await pool.query('SELECT * FROM tipo ');
+    console.log(tipo)
+
+
+    res.render('vistas/verEmpresas', { empresa, tipo });
+
 })
 ////////////////////////////////////////////////////////////////////////////
 router.get('/veamos/:idEmpresa', async (req, res) => {
 
     const servicio1 = await pool.query('SELECT * FROM servicio ');
-    const{idEmpresa}=req.params;
+    const { idEmpresa } = req.params;
 
 
-   
+
     var servicio = [];
     servicio1.reduce(function (contadorAnterior2, actualCuenta) {
         if (actualCuenta.idEmpresa == idEmpresa) {
 
 
 
-           
+
 
             servicio.push(actualCuenta);
-           
-          }
 
-    
-    contadorAnterior2 + 1;
-    }, 0)   
+        }
 
-   
+
+        contadorAnterior2 + 1;
+    }, 0)
+
+
     const empresa = await pool.query('SELECT * FROM empresa WHERE idEmpresa=?', [idEmpresa])
-  
-    
-   
+
+
+
     console.log(servicio)
     /////////////////////////
-   // res.render('vistas/veamos', { empresa: empresa[0]});
-    
-    res.render('vistas/veamos', { empresa: empresa[0], servicio});
+    // res.render('vistas/veamos', { empresa: empresa[0]});
+
+    res.render('vistas/veamos', { empresa: empresa[0], servicio });
 })
+////////////////////////////////busqueda
+
+
+
+
+
+
 
 ///usuariocuentaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 router.get('/usuarioPerfil', async (req, res) => {
-    
+
     res.render('vistas/usuarioPerfil');
 })
 router.get('/empresaPerfil', async (req, res) => {
-    
+
     res.render('vistas/empresaPerfil');
 })
 router.get('/serviciosPerfil', async (req, res) => {
-    
+
     res.render('vistas/serviciosPerfil');
 })
 router.get('/consejosProfile', async (req, res) => {
-    
+
     res.render('vistas/consejosProfile');
 })
 router.get('/funcionamientoProfile', async (req, res) => {
-    
+
     res.render('vistas/funcionamientoProfile');
 })
 router.get('/esferasPerfil', async (req, res) => {
-    
+
     res.render('vistas/esferasPerfil');
 })
 router.get('/informacion', async (req, res) => {
-    
+
     res.render('vistas/informacion');
 })
 router.get('/anuncios', async (req, res) => {
-    
+
     res.render('vistas/anuncios');
 })
+router.post('/verEmpresas/', async (req, res) => {
+
+    console.log('lol')
+    const { idTipo } = req.body;
+    console.log(idTipo)
+    const { valor } = req.body;
+
+
+    const tipo = await pool.query('SELECT * FROM tipo ');
+
+    if (idTipo) {
+        const empresa = await pool.query('SELECT * FROM empresa WHERE idTipo=?', [idTipo])
+        res.render('vistas/busqueda', { empresa,tipo});
+    } else {
+
+        const empresa = await pool.query('SELECT * FROM empresa ');
+        res.render('vistas/busqueda', { empresa,tipo});
+    }
+
+
+})
+
 
 
 
